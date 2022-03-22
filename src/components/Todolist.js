92296d2c3595e45ae8c484c163488c351bcf8f13
yann-soaz <jeff.billot@gmail.com>
2022-03-22 11:51:24 +0100

    ajout css

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,16 +1,31 @@
 import { useState } from "react";
-
+import './Todolist.css';
 
 
 const Todolist = () => {
     let [field, changeField] = useState(''); // useState [string, function ()]
     let [todos, changeTodos] = useState([]); // valeur de l'état, fonction pour une valeur de remplacement
+
+    function changeItemdState (index) { // je me suis rendu compte que j'avais besoin de savoir quel index changer
+        let newArray = [...todos]; // je créer un nouveau tableau qui est une copie de l'ancien
+        // modifications
+            // exemple de données ==> [{name: task1, done: true}, {name: task2, done: false}];
+            // impossible newArray.done
+            // impossible mais trop restrictif newArray[0].done l'index doit etre dynamique
+            // possible newArray[index].done -> création du paramètre
+            if (index < newArray.length && index >= 0) {
+                newArray[index].done = !newArray[index].done;
+            }
+        // end modifs
+        changeTodos(newArray); // j'enregistre le nouveau tableau
+        return; // je mets fin à la fonction
+    }
     return (
         <main>
             <div>
                 <input type="text" value={field} onChange={ (e) => changeField(e.target.value) } />
                 <input type="submit" onClick={ () => {
-                    changeTodos( [ ...todos, field ] );
+                    changeTodos( [ ...todos, {name: field, done: false} ] );
                     changeField('');
                 } } />
             </div>
@@ -19,7 +34,7 @@ const Todolist = () => {
                 {
                     !!todos.length ?
                         todos.map(
-                            (item, index) => <li key={index} >{item}</li> 
+                            ({name, done}, index) => <li key={index} onClick={ () => changeItemdState(index) } className={done ? 'item-done' : 'item'} >{name}</li> 
                         )
                     :
                         <li>j'ai rien</li>
