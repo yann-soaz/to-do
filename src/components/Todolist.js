c9ab493c4280e0890973818b30404d451a11771a
yann-soaz <jeff.billot@gmail.com>
2022-03-22 10:15:33 +0100

    ajout useState

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,10 +1,30 @@
+import { useState } from "react";
+
+
+
 const Todolist = () => {
+    let [field, changeField] = useState(''); // useState [string, function ()]
+    let [todos, changeTodos] = useState([]); // valeur de l'état, fonction pour une valeur de remplacement
     return (
         <main>
-            <form>
-                <input type="text" />
-                <input type="submit" />
-            </form>
+            <div>
+                <input type="text" value={field} onChange={ (e) => changeField(e.target.value) } />
+                <input type="submit" onClick={ () => {
+                    changeTodos( [ ...todos, field ] );
+                    changeField('');
+                } } />
+            </div>
+
+            <ul>
+                {
+                    !!todos.length ?
+                        todos.map(
+                            (item, index) => <li key={index} >{item}</li> 
+                        )
+                    :
+                        <li>j'ai rien</li>
+                }
+            </ul>
         </main>
     )
 }
